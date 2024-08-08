d38801d181a59f31de23c2cbe660e40ff44c3c40
ramprasath31 <117523521+ramprasath31@users.noreply.github.com>
2024-08-08 22:07:08 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import HomePage from './HomePage';
+import InsuranceTypes from './InsuranceTypes';
+import AuthContainer from './AuthContainer';
+import PlansPage from './PlansPage';
+
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/auth" element={<AuthContainer/>}/>
+        <Route path="/insurance-types" element={<InsuranceTypes />} />
+        <Route path="/plans/:insuranceType" element={<PlansPage/>}/>
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
